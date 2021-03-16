6b4333fda155cc5a7109742592255b32354b283c
Rone Silveira <ronebs10@hotmail.com>
2021-03-16 15:37:23 -0300

    structuring the global modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header";
 import Modal from 'react-modal';
 
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -20,13 +21,12 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
-      <Modal 
+
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
+        onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
